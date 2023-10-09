13acac3ff68a4e556fb79b3e4f45415f856fd1df
Nathan Rocha <neroite71@hotmail.com>
2023-10-09 17:48:24 -0300

    Foreign key not null (referential integrity)

---
+++ SalesWebMvc/Migrations/20231009203005_DepartmentForeignKey.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Seller",
+                newName: "DepartmentiD");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentiD");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentiD",
+                table: "Seller",
+                column: "DepartmentiD",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentiD",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentiD",
+                table: "Seller",
+                newName: "DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentiD",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
