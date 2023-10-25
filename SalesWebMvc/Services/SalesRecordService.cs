3cbc725a860322f4827c873fc15bdab49f582136
Nathan Rocha <neroite71@hotmail.com>
2023-10-25 12:19:35 -0300

    minor changes

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -4,13 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
 namespace SalesWebMvc.Services
 {
     public class SalesRecordService
     {
         private readonly SalesWebMvcContext _context;
-
         public SalesRecordService(SalesWebMvcContext context)
         {
             _context = context;
@@ -45,13 +43,12 @@ namespace SalesWebMvc.Services
             {
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
-            return await result
-                .Include(x => x.Seller)
-                .Include(x => x.Seller.Department)
-                .OrderByDescending(x => x.Date)
-                .GroupBy(x => x.Seller.Department)
-                .ToListAsync();
+            var data = await result
+               .Include(x => x.Seller)
+               .Include(x => x.Seller.Department)
+               .OrderByDescending(x => x.Date)
+               .ToListAsync();
+            return data.GroupBy(x => x.Seller.Department).ToList();
         }
-
     }
 }
